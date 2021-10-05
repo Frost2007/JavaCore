e1f48a4e3d3bc526a11a601cfb0945da71fb77a7
Hovhannes Avetisyan <vip.psn2007@mail.ru>
2021-10-05 20:59:16 +0400

    Homework

---
+++ src/edgar/main.java
@@ -0,0 +1,17 @@
+package edgar;
+
+public class main {
+    public static void main(String[] args) {
+        char a;
+        a='A';
+        for (a = 'A' ; a <= 'Z' ; a++) {
+            System.out.println(a);
+            float m= 3.25f;
+
+        }
+
+    }
+}
+
+
+
