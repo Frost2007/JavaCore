530ac9dab4f5b2c30f61a19a3936a51aba9a9c14
Hovhannes Avetisyan <vip.psn2007@mail.ru>
2021-10-06 22:02:02 +0400

    Figure

--- src/Homework/FigurePainter2.java
+++ src/Homework/FigurePainter2.java
@@ -4,9 +4,12 @@ public class FigurePainter2 {
     public static void main(String[] args) {
 
         for (int i = 0; i < 5; i++) {
-            for (int j = 5; j > i + 1; j--) {
+            for (int j = 5; j > i + 1; j-- ) {
+                { System.out.print("* ");
 
-                System.out.print("* ");
+
+
+                }
 
             }
             System.out.println(" ");
