afd3d3a4bcf7d8a4c079614c332dbcbe7713d6b9
Hovhannes Avetisyan <vip.psn2007@mail.ru>
2021-10-06 22:43:51 +0400

    Aded examples

---
+++ src/Lesson4/ClassAverage.java
@@ -0,0 +1,16 @@
+package Lesson4;
+
+public class ClassAverage {
+    public static void main(String[] args) {
+        double nums[] = {10.1, 11.2, 12.3, 13.4, 14.5};
+        double result = 0;
+        int i;
+        for (i = 0; i < 5; i++)
+            result = result = nums[i];
+        System.out.println(
+                " Միջին արժեքը հավասար է " + result / 5
+        );
+
+
+    }
+}
