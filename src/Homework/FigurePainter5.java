07250c4329ed54e9d1d38485fcbd097d13c40c6c
Hovhannes Avetisyan <vip.psn2007@mail.ru>
2021-10-08 00:33:23 +0400

    new homework, exercise 5

---
+++ src/Homework/FigurePainter5.java
@@ -0,0 +1,18 @@
+package Homework;
+
+public class FigurePainter5 {
+    public static void main(String[] args) {
+        for (int i = 1; i <5 ; i++) {
+            {for (int n = 1; n < i; n++)
+                System.out.print("  ");
+                 { for (int j = 5; j >i; j--)
+                     System.out.print(" *");
+
+
+                }
+                System.out.println();
+            }
+            
+        }
+    }
+}
