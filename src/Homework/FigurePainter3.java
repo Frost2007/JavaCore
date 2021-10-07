cc41c45bd930a636fc517142c36daad6885ea20b
Hovhannes Avetisyan <vip.psn2007@mail.ru>
2021-10-08 00:10:38 +0400

    new homework, exercise 4

--- src/Homework/FigurePainter3.java
+++ src/Homework/FigurePainter3.java
@@ -17,7 +17,9 @@ public class FigurePainter3 {
 
         }
 
-
     }
 }
+
+
+
 
---
+++ src/Homework/FigurePainter4.java
@@ -0,0 +1,34 @@
+package Homework;
+
+public class FigurePainter4 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 5; i++) {
+            for (int j = 4; j > i; j--)
+                System.out.print(" ");
+
+            {
+                for (int k = 0; k < i; k++) {
+                    System.out.print(" *");
+
+                }
+
+            }
+            System.out.println();
+
+        }
+        {
+            for (int l = 1; l < 4; l++) {
+                for (int n = 0; n < l; n++)
+                    System.out.print(" ");
+                {
+                    for (int m = 4; m > l; m--)
+                        System.out.print(" *");
+
+                }
+                System.out.println();
+            }
+
+        }
+
+    }
+}
